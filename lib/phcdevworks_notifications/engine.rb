d2c4e49e301e8bb9a889c7d34e62fed80e40e689
BradPotts <brad.potts@phcnet.co>
2019-06-28 13:35:38 +0000

    addin web themes and updated admin themes.

--- lib/phcdevworks_notifications/engine.rb
+++ lib/phcdevworks_notifications/engine.rb
@@ -3,6 +3,7 @@ module PhcdevworksNotifications
 
     # Theme Dependencies
     require "phcthemes_admin_panel_pack"
+    require "phcthemes_web_theme_pack"
 
     # Plugin Namespace
     isolate_namespace PhcdevworksActiveMenus
--- lib/phcdevworks_notifications/version.rb
+++ lib/phcdevworks_notifications/version.rb
@@ -1,3 +1,3 @@
 module PhcdevworksNotifications
-  VERSION = '0.4.0'
+  VERSION = '0.5.0'
 end
